d663dd0fc17c21ecbbe2a2b99daba1fd13e847ab
Francisco <paloufran@gmail.com>
2017-08-09 11:07:33 -0400

    Fix for new version

--- android/src/main/java/cl/json/RNSharePackage.java
+++ android/src/main/java/cl/json/RNSharePackage.java
@@ -15,7 +15,7 @@ public class RNSharePackage implements ReactPackage {
       return Arrays.<NativeModule>asList(new RNShareModule(reactContext));
     }
 
-    @Override
+    //@Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
       return Collections.emptyList();
     }
